cd8565a2582afbafc74f365eabddf3277ad36a67
MrLizzaard <dhk21512@gmail.com>
2022-02-02 11:43:59 +0900

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+const mysql = require("mysql");
+
+const conn = {
+  host: "localhost",
+  port: "3306",
+  user: "user",
+  password: "",
+  database: "opentutorials",
+};
+
+console.log("hello world");
